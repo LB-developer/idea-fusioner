40bb26560b75b6a2d15102ebd0f53adcb6bf42f6
Anahera Foley-Paama <a.foleypaama@gmail.com>
2025-03-10 10:19:09 +1300

    feat: new idea server post request function

--- server/db/data/words.ts
+++ server/db/data/words.ts
@@ -1,7 +1,12 @@
+import { Idea, IdeaData } from '../../../models/ideas.ts'
 import db from '../connection.ts'
 
-export async function getAllWords() {
-    const words = await db('words').select()
-    return words // as type
+export async function getAllIdeas() {
+  const ideas = await db('ideas').select()
+  return ideas as Idea[] // as type
 }
 
+export async function createNewIdea({ idea }: IdeaData) {
+  const result = await db('ideas').insert({ idea }, ['id'])
+  return result[0].id
+}
