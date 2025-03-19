440f9a526dd876e20bb2e34af867337e27e94f2c
lblmdesk <loganbendev@gmail.com>
2025-03-19 15:34:27 +1300

    feat: create a landing page to house components

---
+++ client/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GetIdeasSection from '../../client/components/GetIdeasSection'
+
+export default function LandingPage() {
+
+    return (
+        <>
+            <div>Landing Page</div>
+            <GetIdeasSection />
+        </>
+    )
+}
